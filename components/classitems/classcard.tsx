9b0aede47f2c67a6ee878e662f17b088d484877c
Pernilla Eldh <pernilla.eldh@gmail.com>
2025-10-20 17:01:50 +0200

    Justerat layout för kurskort

--- components/classitems/classcard.tsx
+++ components/classitems/classcard.tsx
@@ -36,7 +36,7 @@ export default function ClassCard({
   school_id: number;
 }) {
   return (
-    <div className="dance-card bg-white rounded-lg overflow-hidden shadow-lg">
+    <div className="grid grid-rows-[auto_1fr] dance-card bg-white rounded-lg overflow-hidden shadow-lg">
       <Image
         className="dark:invert w-full h-48 object-cover"
         src={"/breakdancekids.jpeg"}
@@ -45,7 +45,7 @@ export default function ClassCard({
         height={100}
         priority
       />
-      <div className="p-6 grid h-150">
+      <div className="p-6 grid gap-4">
 
         <div className="card-header flex items-start justify-between">
           <h3 className="text-xl font-bold text-purple-800">{name}</h3>
@@ -56,14 +56,15 @@ export default function ClassCard({
           <p className="text-purple-600 font-medium">{day} kl. {start_time} - {end_time}</p>
           <p className="text-purple-600 font-medium">{level}</p>
         </div>
+        <div>
+          <div className="card-description h-30 overflow-y-auto">
+            <p className="text-gray-700">{description}</p>
+          </div>
 
-        <div className="card-description h-30 overflow-y-auto">
-          <p className="text-gray-700">{description}</p>
           <Link href="/dansstilar" className="text-purple-600 hover:text-purple-800 font-medium">
-            Mer om dansstilen →
+            Mer om {name} →
           </Link>
         </div>
-
         <div className="card-facts space-y-2">
 
           <div className="flex justify-between border-b-2">
