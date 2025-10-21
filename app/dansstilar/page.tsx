2899d0756ffe1df08178e3290f3e02c018f02fd3
Pernilla Eldh <pernilla.eldh@gmail.com>
2025-10-21 16:36:01 +0200

    Färdigställt layout för sidan dansstilar

--- app/dansstilar/page.tsx
+++ app/dansstilar/page.tsx
@@ -1,7 +1,6 @@
 import ContactCards from "@/components/contactitems/contactcards";
 import Footer from "@/components/pagesections/footer";
 import Header from "@/components/pagesections/header";
-import Hero from "@/components/pagesections/hero";
 import Hero2 from "@/components/pagesections/hero2";
 import { Circle } from "lucide-react";
 import Image from "next/image";
@@ -17,7 +16,7 @@ export default async function Dansstilar() {
                 <div className="container mx-auto px-4">
                     <div className="flex flex-col md:flex-row items-center">
                         <div className="md:w-1/2 mb-8 md:mb-0 md:pr-8">
-                            <h2 className="font-nunito text-3xl font-bold text-purple-800 mb-4">Dansmix 1 och 2</h2>
+                            <h2 className="font-nunito text-3xl font-bold text-purple-800 mb-4">Dansmix</h2>
                             <p className="text-gray-700 mb-4">I Dansmix utgår vi från olika dansstilar i olika övningar och koreografier
                                 samt får träna på att dansa i grupp och samarbeta. Vi testar dansstilarna Showjazz, Streetdance och Modern Dans som grund.
                                 I Dansmix 1 lär vi dansens grunder och blandar med lekfullt lärande. I Dansmix 2 får eleverna lite mer utmaning i steggrunder och koreografi.
@@ -124,6 +123,88 @@ export default async function Dansstilar() {
                                 <li className="flex items-center">
                                     <Circle className="fill-purple-700 mr-2" />
                                     Breaking A - Torsdagar kl. 19:00
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="md:w-1/2">
+                            <div className="relative">
+                                <Image
+                                    className="dark:invert rounded-lg shadow-xl w-full"
+                                    src="/breakdancekids.jpeg"
+                                    alt="Barn som dansar breakdance."
+                                    width={500}
+                                    height={100}
+                                    priority
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* Dansstil 4 */}
+            <section className="py-16 bg-gray-100">
+                <div className="container mx-auto px-4">
+                    <div className="flex flex-col md:flex-row items-center">
+                        <div className="md:w-1/2 mb-8 md:mb-0 md:pr-8">
+                            <div className="relative">
+                                <Image
+                                    className="dark:invert object-top-left rounded-lg shadow-xl w-full"
+                                    src="/breakdancekids.jpeg"
+                                    alt="Barn som dansar breakdance."
+                                    width={500}
+                                    height={100}
+                                    priority
+                                />
+                            </div>
+
+                        </div>
+                        <div className="md:w-1/2">
+                            <h2 className="font-nunito text-3xl font-bold text-purple-800 mb-4">Hip Hop</h2>
+                            <p className="text-gray-700 mb-4">Hiphopdans är ett samlingsnamn för alla dansstilar främst utförda till hiphopmusik eller som har
+                                utvecklats som en del av hiphopkulturen. Den innehåller ett brett spektrum av stilar. De ursprungliga danserna samt vad som anses
+                                vara hiphopens grundpelare är Breaking (b-boying), Locking och Popping.
+                            </p>
+                            <div className="flex gap-4">
+                                <p className="text-purple-900 mb-4">
+                                    <Link href="/#schedule" className="hover:underline">Se schema</Link>
+                                </p>
+                                <p className="text-purple-900 mb-4">
+                                    <Link href="/#register" className="hover:underline">Till kursanmälan</Link>
+                                </p>
+                            </div>
+                            <ul className="flex flex-wrap gap-4 list-disc">
+                                <li className="flex items-center">
+                                    <Circle className="fill-purple-700 mr-2" />
+                                    Onsdagar kl. 18:00
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* Dansstil 5 */}
+            <section className="py-16 bg-white">
+                <div className="container mx-auto px-4">
+                    <div className="flex flex-col md:flex-row items-center">
+                        <div className="md:w-1/2 mb-8 md:mb-0 md:pr-8">
+                            <h2 className="font-nunito text-3xl font-bold text-purple-800 mb-4">Locking</h2>
+                            <p className="text-gray-700 mb-4">Locking är en komisk, energisk dansstil, med cartoon-influenser,
+                                inom hiphop- och streetdance-genrerna. Den uppkom i USA på 1960-talet. Stilen har namngetts efter den första lockinggruppen "The Lockers".
+                            </p>
+                            <div className="flex gap-4">
+                                <p className="text-purple-900 mb-4">
+                                    <Link href="/#schedule" className="hover:underline">Se schema</Link>
+                                </p>
+                                <p className="text-purple-900 mb-4">
+                                    <Link href="/#register" className="hover:underline">Till kursanmälan</Link>
+                                </p>
+                            </div>
+                            <ul className="flex flex-wrap gap-4 mt-4 list-disc">
+                                <li className="flex items-center">
+                                    <Circle className="fill-purple-700 mr-2" />
+                                   Lördag kl. 12:00 <span className=" px-4">(OBS! Workshop 1 tillfälle)</span>
                                 </li>
                             </ul>
                         </div>
